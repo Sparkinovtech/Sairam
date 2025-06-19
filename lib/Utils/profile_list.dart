59628ee13332c25f1e01293b90696170edc35fd6
Coder-web-create <johnvick1287@gmail.com>
2025-06-19 09:52:13 +0530

    regular update

---
+++ lib/Utils/profile_list.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:sairam_incubation/Auth/signup_page.dart';
+
+import '../Auth/login_page.dart';
+class ProfileList extends StatefulWidget {
+  const ProfileList({super.key});
+
+  @override
+  State<ProfileList> createState() => _ProfileListState();
+}
+
+class _ProfileListState extends State<ProfileList> with SingleTickerProviderStateMixin {
+
+  late AnimationController _animationController;
+  late Animation<double> _opacity;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(vsync: this , duration: Duration(milliseconds: 700))..repeat(reverse: true);
+    _opacity = Tween<double>(begin: 1.0 , end: 0.2).animate(_animationController);
+  }
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    var size  = MediaQuery.of(context).size;
+    return Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30 , vertical: 15),
+            child: Column(
+              children: [
+                _buildList(icon: Icons.edit, text: "Edit Profile" ,onTap:(){}),
+                SizedBox(height: size.height * .025,),
+                _buildList(icon: CupertinoIcons.lock, text: "Change Password", onTap: (){}),
+                SizedBox(height: size.height *.025,),
+                _buildList(icon: CupertinoIcons.location_solid, text: "Change Location", onTap:(){}),
+                SizedBox(height: size.height * .025,),
+                _buildList(icon: CupertinoIcons.person_2_alt, text: "Clubs and Cells", onTap: (){}),
+                SizedBox(height: size.height * .025,),
+                _buildList(icon: CupertinoIcons.person_3_fill, text: "Other Activities", onTap: (){}),
+                SizedBox(height: size.height * .025,),
+                _buildList(icon: Icons.logout, text: "Log out", onTap:(){
+                  showCupertinoDialog(context: context,barrierDismissible: true  ,builder: (BuildContext context){
+                    return CupertinoAlertDialog(
+                      title: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              AnimatedBuilder(animation: _opacity, builder:(context, child){
+                                return Opacity(opacity: _opacity.value , child: child,);
+                              },
+                                child: Icon(Icons.warning,color: Colors.red,size: 30,),
+
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: size.height* .01,),
+
+                          Text("Are you sure want to log out ?",style: GoogleFonts.inder(color: Colors.black,fontSize: 14,fontWeight: FontWeight.w700),),
+                          SizedBox(height: size.height * .01,),
+                        ],
+                      ),
+                      actions: [
+                        CupertinoDialogAction(
+                          onPressed: (){
+                            Navigator.pushReplacement(context,PageTransition(type: PageTransitionType.fade , duration: Duration(seconds: 1) , child: LoginPage()));
+                          },
+                          child: Text("OK"),
+                        ),
+                        CupertinoDialogAction(
+                          onPressed: (){
+                            Navigator.pop(context);
+                          },
+                          child: Text("CANCEL"),
+                        ),
+
+                      ],
+                    );
+                  });
+                },iconColor: Colors.red),
+                SizedBox(height: size.height * .025,),
+                _buildList(icon: CupertinoIcons.delete_solid, text: "Delete Account", onTap: (){
+                  showCupertinoDialog(context: context,barrierDismissible: false ,builder:(BuildContext context){
+                    return CupertinoAlertDialog(
+                      title: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              AnimatedBuilder(animation: _opacity, builder:(context ,child){
+                                return Opacity(opacity: _opacity.value  , child: child,);
+                                },
+                                child: Icon(Icons.delete_sharp,color: Colors.red,size: 40,),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: size.height * .025,),
+                          Text("Are you sure want to delete account",style:
+                          GoogleFonts.poppins(color: Colors.black,fontSize: 12,fontWeight: FontWeight.w700),),
+                          SizedBox(height: size.height * .025,),
+                          Text("Deleting the account may permanently lose the data !",
+                            style: GoogleFonts.inter(color: Colors.red,fontSize: 13,fontWeight: FontWeight.w500,),),
+                        ],
+                      ),
+                      actions: [
+                        CupertinoDialogAction(
+                          onPressed: (){
+                            Navigator.pushReplacement(context, PageTransition(type: PageTransitionType.fade ,duration: Duration(seconds: 1) ,child: SignupPage()));
+                          },
+                          child: Text("OK",style:GoogleFonts.inter(color: Colors.red,fontWeight: FontWeight.w400),),
+                        ),
+                        CupertinoDialogAction(
+                          onPressed: (){
+                            Navigator.pop(context);
+                          },
+                          child: Text("CANCEL",style: GoogleFonts.inter(color: Colors.black,fontWeight: FontWeight.w400),),
+                        ),
+                      ],
+                    );
+                  });
+                } , iconColor: Colors.red),
+
+              ],
+            ),
+    );
+  }
+  Widget _buildList({required IconData icon , required String text , required VoidCallback onTap , Color iconColor = Colors.black ,Color textColor = Colors.black}){
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(13),
+      ),
+      margin: EdgeInsets.symmetric(vertical: 2),
+      color: Colors.white,
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListTile(
+          onTap: onTap,
+          leading: Icon(icon, color: iconColor,),
+          title: Text(text,style: GoogleFonts.inter(fontSize: 16,fontWeight: FontWeight.w500,color: textColor),),
+          trailing: Icon(Icons.arrow_forward_ios_rounded,color: iconColor),
+        ),
+      ),
+    );
+  }
+}
--- lib/View/profile_page.dart
+++ lib/View/profile_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:sairam_incubation/Utils/profile_list.dart';
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
@@ -31,98 +32,66 @@ class _ProfilePageState extends State<ProfilePage> with SingleTickerProviderStat
       });
     }
   }
+
   @override
   Widget build(BuildContext context) {
     var size  = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
+      body: SafeArea(
         child: Column(
           children: [
-            SizedBox(height: size.height * .1,),
-            Stack(
+            SizedBox(height: size.height * 0.04),
+            Column(
               children: [
-                   Center(
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children : [
-                        Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(85),
-                          ),
-                          elevation: 10,
-                          child: CircleAvatar(radius: 85,backgroundColor: Colors.white,backgroundImage: _imageFile != null ? FileImage(_imageFile!) :
-                          NetworkImage("https://t3.ftcdn.net/jpg/03/02/88/46/360_F_302884605_actpipOdPOQHDTnFtp4zg4RtlWzhOASp.jpg")  as ImageProvider,
-                          ),
-                        ),
-                        Positioned(
-                          bottom: 0,
-                          right: 0,
-                          child: Container(
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.white,
-                            ),
-                            padding: EdgeInsets.all(10),
-                            child: IconButton(onPressed: (){
-                              _openCamera();
-                            }, icon:Icon( CupertinoIcons.camera_fill),
-                            ),
-                          ),
-                        ),
-                      ],
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Card(
+                      elevation: 10,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(80),
+                      ),
+                      color: Colors.white,
+                      child: CircleAvatar(
+                        radius: 80,
+                        backgroundImage: _imageFile != null ? FileImage(_imageFile!) :
+                        NetworkImage("https://t3.ftcdn.net/jpg/03/02/88/46/360_F_302884605_actpipOdPOQHDTnFtp4zg4RtlWzhOASp.jpg") as ImageProvider,
+                        backgroundColor: Colors.white,
+                      ),
                     ),
-                  ),
-              ],
-            ),
-            SizedBox(height: size.height * .03,),
-            Text("John Patric",style: GoogleFonts.inter(fontSize: 21 , fontWeight: FontWeight.bold,color: Colors.black),),
-            SizedBox(height: size.height * .01,),
-            Text("CSE A SEC22CS102",style: GoogleFonts.inter(fontSize: 14,fontWeight: FontWeight.w400,color: Colors.grey),),
-            SizedBox(height: size.height * .03,),
-            ListView(
-              shrinkWrap: true,
-              physics: ScrollPhysics(),
-              padding: EdgeInsets.symmetric(vertical: 20 , horizontal: 20),
-              children: [
-                _buildList(icon: Icons.edit, text:"Edit Profile", onTap: () {
-                }),
-                SizedBox(height: size.height * .025,),
-                _buildList(icon:Icons.password, text: "Change Password", onTap: (){}),
-                SizedBox(height: size.height * .025,),
-                _buildList(icon: CupertinoIcons.location_fill, text: "Change Location", onTap:(){}),
-                SizedBox(height: size.height * .025,),
-                _buildList(icon: CupertinoIcons.person_2_alt, text: "Clubs and Cells ", onTap:(){}),
-                SizedBox(height: size.height * .025,),
-                _buildList(icon: CupertinoIcons.person_3, text: "Other Activities", onTap:(){}),
-                SizedBox(height: size.height * .025,),
-                _buildList(icon: Icons.logout, text: "Log Out", onTap: (){} ,iconColor: Colors.red),
-                SizedBox(height: size.height * .025,),
+                    Positioned(
+                      bottom: 0,
+                      right: size.width * .032,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.photo_camera_solid,color: Colors.black,)),
 
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: size.height * .02,),
+                Text("JOHN PATRIC",style: GoogleFonts.poppins(color: Colors.black,fontSize: 16,fontWeight: FontWeight.bold),),
+                SizedBox(height: size.height * .015,),
+                Text("CSE A SEC22CS23",style: GoogleFonts.inter(color: Colors.grey,fontSize: 14,fontWeight: FontWeight.w700),),
               ],
             ),
+            Expanded(
+              child: ListView(
+                children: [
+                  ProfileList(),
+                ],
+              ),
+            )
           ],
         ),
       ),
     );
   }
 
-  Widget _buildList({required IconData icon , required String text , required VoidCallback onTap , Color iconColor = Colors.black ,Color textColor = Colors.black}){
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(13),
-      ),
-      margin: EdgeInsets.symmetric(vertical: 2),
-      color: Colors.white,
-      elevation: 5,
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListTile(
-          leading: Icon(icon, color: iconColor,),
-          title: Text(text,style: GoogleFonts.inter(fontSize: 16,fontWeight: FontWeight.w500,color: textColor),),
-          trailing: Icon(Icons.arrow_forward_ios_rounded,color: iconColor),
-        ),
-      ),
-    );
-  }
 }
